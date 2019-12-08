24ed1c721b52274ed45b646f12905d0e20d1ef3a
farhad-nstu <amfarhad33@gmail.com>
2019-12-08 18:51:03 +0600

    up to 38

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <add-blog></add-blog>
+    <show-blogs></show-blogs>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+  import addBlog from './components/addBlog.vue'
+  import showBlogs from './components/showBlogs.vue'
+  export default {
+    components: {
+      'add-blog': addBlog,
+      'show-blogs': showBlogs
+    },
+
+    data () {
+
+      return {
+
+      }
+
+    },
+
+    methods: {
+
+    }
+  }
+</script>
+
+<style>
+  body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+  }
+</style>
---
+++ src/components/showBlogs.vue
@@ -0,0 +1,85 @@
+<template>
+    <div v-theme:column="'narrow'" id="show-blogs">
+        <h1>All Blog Articles</h1>
+        <input type="text" v-model="search" placeholder="search blogs" />
+        <div v-for="blog in filteredBlogs" class="single-blog">
+            <h2 v-rainbow>{{ blog.title | toUpperCase }}</h2>
+            <article>{{ blog.body | snippet }}</article>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            blogs: [],
+            search: ''
+        }
+    },
+    methods: {
+    },
+    created() {
+        this.$http.get('http://jsonplaceholder.typicode.com/posts').then(function(data){
+            this.blogs = data.body.slice(0,10);
+        });
+    },
+
+    computed: {
+
+        //Search function start
+        filteredBlogs: function(){
+            return this.blogs.filter((blog) => {
+                return blog.title.match(this.search);
+            });
+        }
+        //Search function end
+    },
+
+    filters: {
+        toUpperCase(value){
+            return value.toUpperCase();
+        },
+        snippet(value){
+            return value.slice(0,200) + '...';
+        }
+    },
+
+    directives: {
+        'rainbow' :{
+            bind(el, binding, vnode){
+                el.style.color = "#" + Math.random().toString(16).slice(2, 8);
+            }
+        },
+
+        'theme' : {
+            bind(el, binding, vnode){
+                if (binding.value == 'wide'){
+                    el.style.maxWidth = "1260px";
+                } else if (binding.value = 'narrow'){
+                    el.style.maxWidth = "560px";
+                }
+                if(binding.arg == 'column'){
+                    el.style.background = '#ddd';
+                    el.style.padding = '20px';
+                }
+            }
+        }
+    }
+
+
+}
+</script>
+
+<style>
+#show-blogs{
+    max-width: 800px;
+    margin: 0px auto;
+}
+.single-blog{
+    padding: 20px;
+    margin: 20px 0;
+    box-sizing: border-box;
+    background: #eee;
+}
+</style>
